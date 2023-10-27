2003220de559598aef3bbe7acc83fdbdff1e4863
pauloperezdev <82291364+pauloperezdev@users.noreply.github.com>
2023-10-27 17:55:33 -0300

    Creado el form

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("tipo-seguro").addEventListener("change", function() {
+    const tipoSeguro = this.value;
+    let valor = 0;
+
+    if (tipoSeguro === "Basico") {
+        valor = 500;
+    } else if (tipoSeguro === "Intermedio") {
+        valor = 1000;
+    } else if (tipoSeguro === "Premium") {
+        valor = 1500;
+    }
+
+    document.getElementById("valor-seguro").textContent = `Valor del seguro: $${valor}`;
+});
