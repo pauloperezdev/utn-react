3ae6f1fb941ee615556bff4475a02f4149df2315
Paulo Perez <sebabueno@gmail.com>
2023-11-07 16:12:19 -0300

    Mejoras pequeñas

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ document.getElementById("tipo-seguro").addEventListener("change", function() {
         valor = 1000;
     } else if (tipoSeguro === "Premium") {
         valor = 1500;
+    } else {
+        valor = 0;
     }
 
     document.getElementById("valor-seguro").textContent = `Valor del seguro: $${valor}`;
